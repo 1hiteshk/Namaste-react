58c67c7883570a267b825176effa56446521a33e
Hitesh Kumar <1hiteshk@gmail.com>
2023-05-11 15:24:25 +0530

    commit day 2

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM  from "react-dom";
+
 const heading = React.createElement("h1",{ id : "heading", xyz:"abc"},"hello world from react ");
 
 const parent = React.createElement("div",{id:"parent"},
